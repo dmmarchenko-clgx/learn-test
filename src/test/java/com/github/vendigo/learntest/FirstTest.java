30a9eba277cd943f8ab2b777a09244c71c9b612d
Dmytro Marchenko <dmmarchenko@corelogic.com>
2018-03-05 10:38:26 +0200

    Random_port

--- src/test/java/com/github/vendigo/learntest/FirstTest.java
+++ src/test/java/com/github/vendigo/learntest/FirstTest.java
@@ -8,7 +8,7 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 import lombok.extern.slf4j.Slf4j;
 
 @ExtendWith(SpringExtension.class)
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @Slf4j
 class FirstTest {
 

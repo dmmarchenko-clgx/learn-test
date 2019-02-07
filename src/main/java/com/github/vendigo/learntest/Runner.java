948ff48ef1aff5bd7a554c400602ceb7904a58dd
Dmytro Marchenko <dmmarchenko@corelogic.com>
2019-02-07 14:14:39 +0200

    Empty project

--- src/main/java/com/github/vendigo/learntest/Runner.java
+++ src/main/java/com/github/vendigo/learntest/Runner.java
@@ -1,7 +1,5 @@
 package com.github.vendigo.learntest;
 
-import java.util.Arrays;
-
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -13,17 +11,7 @@ import lombok.extern.slf4j.Slf4j;
 @AllArgsConstructor
 public class Runner implements CommandLineRunner {
 
-    private final OrdersService ordersService;
-
     @Override
     public void run(final String... args) {
-        final Order order1 = new Order("Dmytro", Arrays.asList("Xiaomi Mi router 3", "Drobak USB Type-C wire"));
-        final Order order2 = new Order("Ivan", Arrays.asList("Samsung Gear 3 Sport", "Samsung Galaxy Note 8"));
-        final Order order3 = new Order("Petro", Arrays.asList("Samsung Gear 4", "Samsung Galaxy Note 9"));
-        ordersService.put(1L, order1);
-        ordersService.put(2L, order2);
-        ordersService.put(3L, order3);
-
-        //ordersService.deleteAll();
     }
 }
